7a374dd7f261b3ecd009e49173922cfde0400a85
maolinml <86260930+maolinml@users.noreply.github.com>
2026-03-24 14:34:14 -0700

    add native bp result

--- src/LatticeAlgorithms.jl
+++ src/LatticeAlgorithms.jl
@@ -153,7 +153,7 @@ export bb_X_logicals, bb_Z_logicals
 export bb_Mq, bb_Mp, bb_M
 include("bivariate_bicycle_code.jl")
 
-export bp_osd_cs_decode
+export bp_osd_cs_decode, bp_min_sum_decode
 include("bp_osd_cs.jl")
 
 end # module
