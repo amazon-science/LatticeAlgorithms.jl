032aec49f0719d7765b3d8f1fd68d45260654bbb
maolinml <86260930+maolinml@users.noreply.github.com>
2026-03-28 19:46:47 -0700

    export css_bp_osd_cs_decode

--- src/LatticeAlgorithms.jl
+++ src/LatticeAlgorithms.jl
@@ -154,7 +154,7 @@ export bb_X_logicals, bb_Z_logicals
 export bb_Mq, bb_Mp, bb_M
 include("bivariate_bicycle_code.jl")
 
-export bp_osd_cs_decode, bp_min_sum_decode
+export bp_osd_cs_decode, bp_min_sum_decode, css_bp_osd_cs_decode
 include("bp_osd_cs.jl")
 
 export dmem_bp_decode, relay_bp_decode, css_relay_bp_decode
